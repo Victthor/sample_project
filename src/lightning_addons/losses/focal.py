2dbab7abd8411c6fdb479990923f5bf355ec53f5
victthor <victoregorov23@gmail.com>
2023-11-30 14:42:24 +0200

    continuing building project structure..

--- src/lightning_addons/losses/focal.py
+++ src/lightning_addons/losses/focal.py
@@ -18,7 +18,32 @@ def multiclass_focal_loss(
         reduction: Literal['mean', 'sum', 'none'] = 'mean',
         temperature: Optional[float] = None,  # todo: separate temp of focal term and cross entropy term
 ) -> Tensor:
+    """
+    Compute the multiclass focal loss.
 
+    Parameters:
+        inputs (Tensor): Logits predicted by the model.
+        targets (Tensor): Ground truth class labels.
+        weight (Optional[Tensor]): Weight tensor, typically representing class weights (optional).
+        ignore_index (int): Index to ignore in the loss calculation.
+        gamma (float): Focusing parameter for the focal term.
+        reduction (Literal['mean', 'sum', 'none']): Specifies the reduction to apply to the loss.
+            Options: 'mean', 'sum', 'none'.
+        temperature (Optional[float]): Temperature parameter for adjusting the logits (optional).
+
+    Returns:
+        Tensor: Computed focal loss.
+
+    Notes:
+        If `inputs` has more than two dimensions, it is reshaped to (N * d1 * ... * dK, C), where N is the
+        number of samples, and C is the number of classes. The targets are also reshaped accordingly.
+
+        If `ignore_index` is specified, positions where labels equal `ignore_index` are masked out.
+
+        If `temperature` is provided, the logits are divided by the temperature value.
+
+        The focal loss is calculated as a combination of negative log-likelihood (nll) loss and a focal term.
+    """
     if inputs.ndim > 2:
         # (N, C, d1, d2, ..., dK) --> (N * d1 * ... * dK, C)
         c = inputs.shape[1]
@@ -59,7 +84,23 @@ def multiclass_focal_loss(
 
 
 class MulticlassFocalLoss(torch.nn.Module):
+    """
+    PyTorch module for computing the multiclass focal loss.
 
+    Methods:
+        forward(inputs: Tensor, targets: Tensor) -> Tensor:
+            Forward pass of the module. Computes the multiclass focal loss using the `multiclass_focal_loss` function.
+
+    Attributes:
+        weight (Optional[Tensor]): Weight tensor, typically representing class weights (optional).
+        ignore_index (Optional[int]): Index to ignore in the loss calculation (optional).
+        gamma (float): Focusing parameter for the focal term.
+        reduction (Literal['mean', 'sum', 'none']): Specifies the reduction to apply to the loss.
+        temperature (Optional[float]): Temperature parameter for adjusting the logits (optional).
+
+    Notes:
+        The module wraps the `multiclass_focal_loss` function and allows using it as a part of PyTorch model architecture.
+    """
     def __init__(
             self,
             weight: Optional[Tensor] = None,  # alpha
@@ -68,6 +109,17 @@ class MulticlassFocalLoss(torch.nn.Module):
             reduction: Literal['mean', 'sum', 'none'] = 'mean',
             temperature: Optional[float] = None,
     ):
+        """
+        Constructor for the MulticlassFocalLoss module.
+
+        Parameters:
+            weight (Optional[Tensor]): Weight tensor, typically representing class weights (optional).
+            ignore_index (Optional[int]): Index to ignore in the loss calculation (optional).
+            gamma (float): Focusing parameter for the focal term.
+            reduction (Literal['mean', 'sum', 'none']): Specifies the reduction to apply to the loss.
+                Options: 'mean', 'sum', 'none'.
+            temperature (Optional[float]): Temperature parameter for adjusting the logits (optional).
+        """
         super().__init__()
 
         self.weight = weight
@@ -77,6 +129,19 @@ class MulticlassFocalLoss(torch.nn.Module):
         self.temperature = temperature
 
     def forward(self, inputs: Tensor, targets: Tensor) -> Tensor:
+        """
+        Forward pass of the MulticlassFocalLoss module.
+
+        Parameters:
+            inputs (Tensor): Logits predicted by the model.
+            targets (Tensor): Ground truth class labels.
+
+        Returns:
+            Tensor: Computed focal loss.
+
+        Notes:
+            The forward pass uses the `multiclass_focal_loss` function to compute the loss.
+        """
         return multiclass_focal_loss(
             inputs,
             targets,
